f490c1124b42118ef4d8c71f453d4b91c113ec9c
Umaha Tokula <umahatokula@gmail.com>
2024-07-21 17:42:01 +0100

    User and Profile migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,21 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
         Set::factory(10)->create();
         Center::factory(10)->create();
         C3::factory(10)->create();
         ServiceTeam::factory(10)->create();
         Assessment::factory(3)->create();
-        Profile::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Profile::factory(10)->create();
 
         $this->call([
-            RolesAndPermissionsSeeder::class
+            RolesAndPermissionsSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
